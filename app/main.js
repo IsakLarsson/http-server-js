860c07dd86b685f3381f1a4e9479e0c2a9b4cba9
Isak Åman Larsson <isak.larsson@savr.com>
2024-06-17 17:23:00 +0200

    routes and stuff

--- app/main.js
+++ app/main.js
@@ -1,14 +1,20 @@
-const net = require("net");
+const net = require("node:net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-    socket.on("data", (data) => {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    });
     socket.on("close", () => {
         socket.end();
+        server.close();
+    });
+    socket.on("data", (data) => {
+        const target = data.toString().split(" ")[1];
+        if (target !== "/") {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }
     });
 });
 
