6b505dd92784fa3ab37a6107b2cb56e9cccf06c3
Isak Åman Larsson <isak.larsson@savr.com>
2024-06-17 14:50:49 +0200

    best guess

--- app/main.js
+++ app/main.js
@@ -7,6 +7,9 @@ const server = net.createServer((socket) => {
     socket.on("close", () => {
         socket.end();
     });
+    socket.on("data", (data) => {
+        socket.write("HTTP/1.1 200 OK\r\n");
+    });
 });
 
 server.listen(4221, "localhost");
