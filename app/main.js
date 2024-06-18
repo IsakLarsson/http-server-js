7fc89089a796fade5a47c5f755aec21d4222cbe4
Isak Åman Larsson <isak.larsson@savr.com>
2024-06-18 09:48:04 +0200

    fix

--- app/main.js
+++ app/main.js
@@ -16,7 +16,7 @@ const server = net.createServer((socket) => {
             const content = url.split("/echo/")[1];
             console.log(content);
             socket.write(
-                `HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nContent-Length: ${content.length}\r\n\r\n${content}`,
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`,
             );
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
