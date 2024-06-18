0c03bdc38b217beefe0bae8d449b5cd6d0bb39ce
Isak Åman Larsson <isak.larsson@savr.com>
2024-06-18 09:45:53 +0200

    echo route

--- app/main.js
+++ app/main.js
@@ -9,11 +9,17 @@ const server = net.createServer((socket) => {
         server.close();
     });
     socket.on("data", (data) => {
-        const target = data.toString().split(" ")[1];
-        if (target !== "/") {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        } else {
+        const url = data.toString().split(" ")[1];
+        if (url === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else if (url.includes("/echo/", 0)) {
+            const content = url.split("/echo/")[1];
+            console.log(content);
+            socket.write(
+                `HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nContent-Length: ${content.length}\r\n\r\n${content}`,
+            );
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
     });
 });
